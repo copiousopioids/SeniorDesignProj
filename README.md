d91e6dc8b1a2d50dc3ec971eefde9cb6d11d54e9
zmarcolesco <zmarcolesco@gmail.com>
2018-05-11 16:14:55 -0500

    Added README

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Senior Design Project
+<snippet>
+  <content><![CDATA[
+# ${1:sWiFixer: A WiFi Heatmapper using Python}
+
+The purpose of this project is to measure, store, and map wireless signal strength both spatially and over time. By measuring RSSI of the desired network at fixed or dynamic locations, one can build a visual diagram of the signal over a given space. This technology could have many use cases, from IT, to network planning, or even network security and monitoring. A robust program with these goals could save time and money for many organizations.
+
+## Installation
+
+Make sure you have the latest stable version of python 3.
+
+Python Libraries:
+ - matplotlib
+ - pandas
+ - scipy
+
+## Usage
+
+Run the Server:
+```
+python manage.py runserver
+```
+Run a test of the Heatmapper:
+```
+python heatmap.py "input/apartment-trans.png" "input/apartment.csv" "output/apartment-map.png"
+```
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## Credits
+
+Beau Gunderson (HeatMap base algorithm)
+
+## License
+
+TODO: Write license
--- swifinder/heatmap.py
+++ swifinder/heatmap.py
@@ -9,43 +9,9 @@ from mpl_toolkits.axes_grid1 import AxesGrid
 from scipy.interpolate import Rbf
 from pylab import imread, imshow
 
+# typed as an array to allow for multiple floors/readings later on
+sources = ['rssi']
 
-# layout_name = "Layout"
-#
-# layout = imread('input/%s.png' % (layout_name))
-
-# a = tb.tabarray(SVfile='input/mapping.csv')
-# a = pd.read_csv('input/newmapping.csv')
-
-s_beacons = ['rssi']
-# strengths = 'rssi'
-
-# grid_width = 797
-# grid_height = 530
-#
-# image_width = 2544
-# image_height = 1691
-#
-# grid_width = image_width // 3.2
-# grid_height = image_height // 3.2
-
-# num_x = image_width / 4
-# num_y = num_x / (image_width / image_height)
-
-# num_x = image_width // 8
-# num_y = num_x // (image_width // image_height)
-#
-# print("Resolution: %0.2f x %0.2f" % (num_x, num_y))
-#
-# x = np.linspace(0, grid_width, num_x)
-# y = np.linspace(0, grid_height, num_y)
-#
-# gx, gy = np.meshgrid(x, y)
-# gx, gy = gx.flatten(), gy.flatten()
-#
-# levels = [-85, -80, -75, -70, -65, -60, -55, -50, -45, -40, -35, -30, -25]
-#
-# interpolate = True
 
 def add_inner_title(ax, title, loc, size=None, **kwargs):
     from matplotlib.offsetbox import AnchoredText
@@ -90,55 +56,53 @@ def save_single_plot(layout_fn, csv_fn, output_fn):
     gx, gy = np.meshgrid(x, y)
     gx, gy = gx.flatten(), gy.flatten()
 
+
+    # -80dBm: Unstable; -30dBm: Perfect signal
+    # Source: https://eyesaas.com/wi-fi-signal-strength/
     levels = [-85, -80, -75, -70, -65, -60, -55, -50, -45, -40, -35, -30, -25]
 
     interpolate = True
 
     f = pp.figure()
 
-    # f.suptitle("RSSI Heatmap")
-
-    # Adjust the margins and padding
-    # f.subplots_adjust(hspace=0.1, wspace=0.1, left=0.05, right=0.95, top=0.85,
-    #         bottom=0.15)
-
-    # Create a grid of subplots using the AxesGrid helper
+    # Use AxesGrid to create subplots
+    # Subplots not used, but useful for multiple floors
     image_grid = AxesGrid(f, 111, nrows_ncols=(1, 1), axes_pad=0.1,
             label_mode="1", share_all=True, cbar_location="right",
             cbar_mode="single", cbar_size="3%")
 
-    for beacon, i in zip(s_beacons, range(len(s_beacons))):
-        # Hide the axis labels
+    for beacon, i in zip(sources, range(len(sources))):
+        # Hide labels
         image_grid[i].xaxis.set_visible(False)
         image_grid[i].yaxis.set_visible(False)
 
         if interpolate:
             # Interpolate the data
             rbf = Rbf(a['Drawing X'], a['Drawing Y'], a[beacon],
-                    function='linear')
+                    function='linear') #possibly logarithmic?
 
+            # Run rbf() on the data to find the interpolated value
             z = rbf(gx, gy)
             z = z.reshape((num_y, num_x))
 
-            # Render the interpolated data to the plot
+            # Render the interpolated data (red, yellow, blue)
             image = image_grid[i].imshow(z, vmin=-85, vmax=-25, extent=(0,
                 image_width, image_height, 0), cmap='RdYlBu_r', alpha=1)
 
-            #c = image_grid[i].contourf(z, levels, alpha=0.5)
-            #c = image_grid[i].contour(z, levels, linewidths=5, alpha=0.5)
         else:
+            # If value not interpolated
             z = ml.griddata(a['Drawing X'], a['Drawing Y'], a[beacon], x, y)
 
             c = image_grid[i].contourf(x, y, z, levels, alpha=0.5)
 
         image_grid[i].imshow(layout, interpolation='bicubic', zorder=100)
 
-    # Setup the data for the colorbar and its ticks
+    # colorbar setup
     image_grid.cbar_axes[0].colorbar(image)
     image_grid.cbar_axes[0].set_yticks(levels)
 
-    # Add inset titles to each subplot
-    for ax, im_title in zip(image_grid, s_beacons):
+    # Add titles
+    for ax, im_title in zip(image_grid, sources):
         t = add_inner_title(ax, "Layout Name: %s" % layout_fn, loc=3)
 
         t.patch.set_alpha(0.5)
@@ -150,7 +114,3 @@ if __name__ == "__main__":
     import sys
     print(sys.argv)
     save_single_plot(sys.argv[1], sys.argv[2], sys.argv[3]);
-
-    # save_single_plot("input/Layout.png", "input/newmapping.csv", "output/test.png");
-    # grid_plots()
-    # max_plot()
